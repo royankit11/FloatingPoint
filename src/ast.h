fbff431b006ed52ef530dab949d8d85c723777ae
royankit11 <68195611+royankit11@users.noreply.github.com>
2022-08-16 10:30:41 -0400

    Update ast.h
    Condensed nodes into binary and unary

--- src/ast.h
+++ src/ast.h
@@ -15,199 +15,77 @@ class Expression {
 public:
   virtual ~Expression () {}
 
-  //  It's necessary because we need to clone objects without
-  // knowing the exact type.
-  virtual Expression *clone () = 0;
-
   // The value represented by the expression
-  virtual int value () = 0;
-};
-
-// For addictive expressions
-class Plus : public Expression {
-  Expression *m_left, *m_right;
-
-public:
-   
-  Plus (Expression *left, Expression *right): m_left (left), m_right (right) {}
-
-  // Copy constructor
-  Plus (const Plus &other) {
-    m_left = other.m_left->clone ();
-    m_right = other.m_right->clone ();
-  }
-  
-  virtual ~Plus ()
-  {
-    delete m_left;
-    delete m_right;
-  }
-
-  Plus &operator = (const Plus &other) {
-    if (&other != this) {
-      delete m_left;
-      delete m_right;
-      
-      m_left = other.m_left->clone ();
-      m_right = other.m_right->clone ();
-    }
-  }
-  
-
-  virtual Expression *clone () { return new Plus (*this); }
-  
-  virtual int value () { return m_left->value () + m_right->value (); }
-  
-};
-
-// For multiplicative expressions
-class Times : public Expression {
-  Expression *m_left, *m_right;
-
-public:
-   
-  Times (Expression *left, Expression *right): m_left (left), m_right (right) {}
-
-  // Copy constructor
-  Times (const Times &other) {
-    m_left = other.m_left->clone ();
-    m_right = other.m_right->clone ();
-  }
-
-  virtual ~Times ()
-  {
-    delete m_left;
-    delete m_right;
-  }
-
-  Times &operator = (const Times &other) {
-    if (&other != this) {
-      delete m_left;
-      delete m_right;
-      
-      m_left = other.m_left->clone ();
-      m_right = other.m_right->clone ();
-    }
-  }
-  
-
-  virtual Expression *clone () { return new Times (*this); }
-  
-  virtual int value () { return m_left->value () * m_right->value (); }
-  
-};
-
-class Subtract : public Expression {
-  Expression *m_left, *m_right;
-
-public:
-   
-  Subtract (Expression *left, Expression *right): m_left (left), m_right (right) {}
-
-  // Copy constructor
-  Subtract (const Subtract &other) {
-    m_left = other.m_left->clone ();
-    m_right = other.m_right->clone ();
-  }
-
-  virtual ~Subtract ()
-  {
-    delete m_left;
-    delete m_right;
-  }
-
-  Subtract &operator = (const Subtract &other) {
-    if (&other != this) {
-      delete m_left;
-      delete m_right;
-      
-      m_left = other.m_left->clone ();
-      m_right = other.m_right->clone ();
-    }
-  }
-  
-
-  virtual Expression *clone () { return new Subtract (*this); }
-  
-  virtual int value () { return m_left->value () - m_right->value (); }
-  
-};
-
-class Divide : public Expression {
-  Expression *m_left, *m_right;
-
-public:
-   
-  Divide (Expression *left, Expression *right): m_left (left), m_right (right) {}
-
-  // Copy constructor
-  Divide (const Divide &other) {
-    m_left = other.m_left->clone ();
-    m_right = other.m_right->clone ();
-  }
-
-  virtual ~Divide ()
-  {
-    delete m_left;
-    delete m_right;
-  }
-
-  Divide &operator = (const Divide &other) {
-    if (&other != this) {
-      delete m_left;
-      delete m_right;
-      
-      m_left = other.m_left->clone ();
-      m_right = other.m_right->clone ();
-    }
-  }
-  
-
-  virtual Expression *clone () { return new Divide (*this); }
-  
-  virtual int value () { return m_left->value () / m_right->value (); }
-  
+    virtual int value () = 0;
 };
 
 // For numbers
 class Number : public Expression {
-  int m_val;
+    int m_val;
 
-public:
+    public:
    
-  Number (int val): m_val (val) {}
+        Number (int val): m_val (val) {}
 
-  // Copy constructor
-  Number (const Number &other) { m_val = other.m_val; }
-
-  Number &operator = (const Number &other) {
-    if (&other != this)
-      m_val = other.m_val;
-  }
-
-  virtual Expression *clone () { return new Number (*this); }
-
-  virtual int value () { return m_val; }
-};
+        virtual int value () { return m_val; }
+    };
 
 // For identifiers
 class Ident : public Expression {
-  int *m_val;
+    int m_val;
 
-public:
+    public:
    
-  Ident (int *val): m_val (val) {}
-
-  // Copy constructor
-  Ident (const Ident &other) { m_val = other.m_val; }
-
-  Ident &operator = (const Ident &other) {
-    if (&other != this)
-      m_val = other.m_val;
-  }
-
-  virtual Expression *clone () { return new Ident (*this); }
+        Ident (int val): m_val (val) {}
   
 
-  virtual int value () { return *m_val; }
+        virtual int value () { return m_val; }
 };
+
+
+// For binary expressions
+class Binary : public Expression {
+    Expression *m_left, *m_right;
+    char m_oper;
+
+    public:
+   
+        Binary (Expression *left, char oper, Expression *right): m_left (left), m_oper (oper), m_right (right) {}
+
+   
+        virtual int value () {
+            if (m_oper == '+') {
+                return m_left->value () + m_right->value ();
+            } else if (m_oper == '*') {
+                return m_left->value () * m_right->value ();
+            } else if (m_oper == '-') {
+                return m_left->value () - m_right->value ();
+            } else {
+                return m_left->value () / m_right->value ();
+            }
+            
+        }
+    
+};
+//unary expressions
+class Unary : public Expression {
+    Expression *m_right;
+    char m_oper;
+
+    public:
+   
+        Unary (Expression *right, char oper): m_oper (oper), m_right (right) {}
+
+   
+        virtual int value () {
+            if (m_oper == '+') {
+                return m_right->value ();
+            } else {
+                return m_right->value ();
+            }
+            
+        }
+    
+};
+
+
