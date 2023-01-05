dacb2c691acbf5156847652772e94f4a46339901
royankit11 <68195611+royankit11@users.noreply.github.com>
2023-01-05 15:32:46 -0500

    Update ast.h
    Added some functionality to evaluate function

--- src/ast.h
+++ src/ast.h
@@ -7,11 +7,9 @@
 struct Expression {
 public:
   virtual ~Expression () {}
-    virtual std::string evaluate () = 0;
+    virtual std::string toString () = 0;
+    virtual int evaluate () = 0;
 
-  // The value represented by the expression
-
-    //virtual std::string repeat () = "";
 };
 
 // For numbers
@@ -22,7 +20,9 @@ public:
    
     Number (int val): m_val (val) {}
 
-    virtual  std::string evaluate () { return std::to_string(m_val); }
+    virtual std::string toString () { return std::to_string(m_val); }
+    virtual int evaluate () {return m_val;};
+    
 };
 
 // For identifiers
@@ -36,7 +36,8 @@ public:
         //std::cout << *m_val;
     }
 
-    virtual std::string evaluate () { return *m_val;}
+    virtual std::string toString () { return *m_val;}
+    virtual int evaluate () {return 0;};
 };
 
 
@@ -50,18 +51,30 @@ public:
     Binary (Expression *left, char oper, Expression *right): m_left (left), m_oper (oper), m_right (right) {}
 
 
-    virtual std::string evaluate () {
+    virtual std::string toString () {
         if (m_oper == '+') {
-            return m_left->evaluate () + "+" + m_right->evaluate ();
+            return m_left->toString () + "+" + m_right->toString ();
         } else if (m_oper == '*') {
-            return m_left->evaluate () + "*" + m_right->evaluate ();
+            return m_left->toString () + "*" + m_right->toString ();
         } else if (m_oper == '-') {
-            return m_left->evaluate () + "-" + m_right->evaluate ();
+            return m_left->toString () + "-" + m_right->toString ();
         } else {
-            return m_left->evaluate () + "/" + m_right->evaluate ();
+            return m_left->toString () + "/" + m_right->toString ();
         }
         
     }
+    virtual int evaluate () {
+        if (m_oper == '+') {
+            return m_right->evaluate() + m_left->evaluate();
+        } else if (m_oper == '*') {
+            return m_right->evaluate() * m_left->evaluate();
+        } else if (m_oper == '-') {
+            return m_right->evaluate() - m_left->evaluate();
+        } else {
+            return m_right->evaluate() / m_left->evaluate();
+        }
+        
+    };
     
 };
 //unary expressions
@@ -74,14 +87,22 @@ public:
     Unary (Expression *right, char oper): m_oper (oper), m_right (right) {}
 
 
-    virtual std::string evaluate () {
+    virtual std::string toString () {
         if (m_oper == '+') {
-            return m_right->evaluate ();
+            return m_right->toString ();
         } else {
-            return "-" + m_right->evaluate ();
+            return "-" + m_right->toString ();
         }
         
     }
+    virtual int evaluate () {
+        if (m_oper == '+') {
+            return m_right->evaluate();
+        } else {
+            return  0 - + m_right->evaluate ();
+        }
+        
+    };
     
 };
 
@@ -95,9 +116,10 @@ public:
         }
 
 
-    virtual std::string evaluate () {
-        return *m_var + "=" + m_right->evaluate();
+    virtual std::string toString () {
+        return *m_var + "=" + m_right->toString();
         
     }
+    virtual int evaluate () {return 0;};
     
 };
